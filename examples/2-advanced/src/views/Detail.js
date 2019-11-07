bdd0ec227b535fb6e8d80c0bd6d77442ed0cdb6e
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-11-06 18:03:23 -0600

    remove async call from Detail component

--- examples/2-advanced/src/views/Detail.js
+++ examples/2-advanced/src/views/Detail.js
@@ -2,7 +2,6 @@ import { h } from 'preact';
 import { useEffect, useRef } from 'preact/hooks';
 import { Container } from '../components';
 import { setTheme } from '../lib/theme';
-import ImageTools from '../lib/image-tools';
 import './Detail.scss';
 
 const Detail = ({ imageId }) => {
@@ -10,19 +9,12 @@ const Detail = ({ imageId }) => {
   const imageUrl = `https://picsum.photos/id/${imageId}/640/480`;
 
   useEffect(() => {
-    // Instatiate our Comlink class asynchronously
-    new ImageTools()
-      .then(imageTools => imageTools.loadImage(imageUrl)) // invoke the instance's `loadImage` method
-      .then(result => {
-        const hsl = result.color.hsl
-        const image = imageRef.current
-        image.src = URL.createObjectURL(result.blob) // create an object URL from the blob and set it as image source
-        setTheme({
-          hue: hsl.h,
-          saturation: hsl.s,
-          luminosity: hsl.l
-        });
-      });
+    // ! Load our image in a Web Worker, get its dominant color and set our theme with the color
+    // setTheme({
+    //   hue: h,
+    //   saturation: s,
+    //   luminosity: l
+    // });
   });
 
   return (
